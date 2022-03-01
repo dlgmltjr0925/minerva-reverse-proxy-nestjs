63049262430988b0f0b96a65b616fa532e183c98
fine <dlgmltjr0925@gmail.com>
2022-03-02 03:56:44 +0900

    Axios 조회 기능 구현

--- src/reverse/reverse.module.ts
+++ src/reverse/reverse.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ReverseController } from './reverse.controller';
 import { ReverseService } from './reverse.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [ReverseController],
   providers: [ReverseService],
 })
--- src/reverse/reverse.service.ts
+++ src/reverse/reverse.service.ts
@@ -1,24 +1,85 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'purge'
+  | 'PURGE'
+  | 'link'
+  | 'LINK'
+  | 'unlink'
+  | 'UNLINK';
+
+export type AxiosRequestHeaders = Record<string, string | number | boolean>;
+
+interface AxiosRequestConfig {
+  method: Method;
+  url: string;
+  params: any;
+  data?: any;
+  headers: any;
+}
 @Injectable()
 export class ReverseService {
-  get() {
-    return 'get';
+  constructor(private httpService: HttpService) {}
+
+  getAxiosRequestConfigByRequest(
+    { method, params, query, headers }: Request,
+    body?: any,
+  ): AxiosRequestConfig {
+    delete headers.host;
+
+    return {
+      method: method as Method,
+      url: params[0],
+      params: query,
+      data: body,
+      headers: {
+        via: 'minerva reverse proxy/1.0.0',
+        ...headers,
+      },
+    };
   }
 
-  post() {
-    return 'post';
+  async request(req: Request, body?: any) {
+    const { data } = await this.httpService
+      .request(this.getAxiosRequestConfigByRequest(req, body))
+      .toPromise();
+
+    return data;
   }
 
-  patch() {
-    return 'patch';
+  async get(req: Request): Promise<any> {
+    return await this.request(req);
   }
 
-  put() {
-    return 'put';
+  async post(req: Request, body: any) {
+    return await this.request(req, body);
   }
 
-  delete() {
-    return 'delete';
+  async patch(req: Request, body: any) {
+    return await this.request(req, body);
+  }
+
+  async put(req: Request, body: any) {
+    return await this.request(req, body);
+  }
+
+  async delete(req: Request) {
+    return await this.request(req);
   }
 }
